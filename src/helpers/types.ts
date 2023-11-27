fc4577dfc3d4952d9cdd6f2ac92ca4b5b88afd88
DESKTOP-KENRFDR\Malisha <malishamaharjan00@gmail.com>
2023-11-27 21:31:54 +0545

    types

---
+++ src/helpers/types.ts
@@ -0,0 +1,10 @@
+export type Todo = {
+  created_at: Date;
+  description: string;
+  id: number;
+  is_completed: boolean;
+  schedule_at: Date;
+  task: string;
+  user: number;
+  index: number;
+};
